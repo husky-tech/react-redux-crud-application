f3b9730d2b046277de26e016ec26230b229fddd0
wakebear <wakesubad@gmail.com>
2018-07-10 11:13:42 +0900

    create test component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
+        <div>
           <h1>Hello World</h1>
+          <App2 />
+          <Cat />
+        </div>
 
     );
   }
+}
+
+const App2 = () => {
+  return(
+    <div>
+      <h1>へーい</h1>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
+}
+
+const Cat = () => {
+  return <h2>おれはねこ</h2>
 }
 
 export default App;
