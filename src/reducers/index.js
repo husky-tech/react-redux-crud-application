148f73fcacfe7464c2082a6682006c9f3e7e9639
wakebear <wakesubad@gmail.com>
2018-07-17 14:51:39 +0900

    implement event new and action create

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 //全てのReducersを総括する　パッケージ必要
 
 import { combineReducers } from 'redux'
+import { reducer as form } from 'redux-form'
 import events from './events'
 
 
-export default combineReducers({ events })
+export default combineReducers({ events, form })
 //今回は状態を管理するのは１つだけ
